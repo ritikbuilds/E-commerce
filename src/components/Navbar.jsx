0f2e5e87c6e787d397c688bbc0f52329abc9c03a
Ritik <ritikprajapati2704@gmail.com>
2024-07-06 00:08:42 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -57,7 +57,7 @@ function Navbar({ openModal }) {
         <input
           name="search"
           type="text"
-          className="p-2 flex-1 rounded-l-lg outline-none w-[12rem] sm:w-full"
+          className="p-2 flex-1 text-sm sm:text-md rounded-l-lg outline-none w-[12rem] sm:w-full"
           placeholder="Search Products here"
           value={searchValue}
           onChange={(e) => {
